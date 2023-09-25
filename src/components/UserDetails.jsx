0b0686b0992f5cef8a28fab4c816380f4fcae90f
wahab315 <abdulwahab315tts@gmail.com>
2023-09-25 18:29:37 +0500

    new code

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import { DeleteAllUser } from "./DeleteAllUser";
 import styled from "styled-components";
+import { fakeData } from "../data";
+
+import { addUser } from "../store/slices/UserSlice";
+import { useDispatch } from "react-redux";
+import Display from "./Display";
+
+import { v4 as uuidv4 } from "uuid";
 
 const UserDetails = () => {
+  const dispatch = useDispatch();
+  function addNewUser() {
+    dispatch(
+      addUser({
+        id: uuidv4(),
+        name: fakeData(),
+      })
+    );
+  }
+
   return (
     <Wrapper>
       <div className="content">
         <div className="admin-table">
           <div className="admin-subtitle">List of User Details</div>
-          <button className="btn add-btn">Add New Users</button>
+          <button className="btn add-btn" onClick={addNewUser}>
+            Add New Users
+          </button>
         </div>
         <ul>
-          {/* <li>Hi</li>
-          <li>Hii</li> */}
+          <Display />
         </ul>
         <hr />
         <DeleteAllUser />
